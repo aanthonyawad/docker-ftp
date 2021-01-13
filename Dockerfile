183cb69df3363a7a00be699d4f21fbbdf1c5892c
aanthonyawad <dev.anthony.awad@gmail.com>
2021-01-13 20:33:19 +0200

    config update

--- Dockerfile
+++ Dockerfile
@@ -4,5 +4,7 @@ RUN apk --no-cache add vsftpd
 COPY vsftpd.sh /bin/vsftpd.sh
 COPY vsftpd.conf /etc/vsftpd/vsftpd.conf
 # COPY vsftpd.userlist /etc/vsftpd/vsftpd.userlist
+EXPOSE 21 21000-21010
+# VOLUME /ftp/ftp
 
 ENTRYPOINT ["/bin/vsftpd.sh","anthony","1234"]
--- vsftpd.sh
+++ vsftpd.sh
@@ -4,21 +4,17 @@
 #Remove all ftp users
 grep '/ftp/' /etc/passwd | cut -d':' -f1 | xargs -n1 deluser
 
- NAME=${1}
- PASS=${2}
-
-
+NAME=${1}
+PASS=${2}
+# UID = ${3}
 FOLDER="/ftp/$NAME/files"
+UID_OPT="-u 1000"
 
-if [ ! -z "$UID" ]; then
-UID_OPT="-u $UID"
-fi
 
-echo -e "$PASS\n$PASS" | adduser -h $FOLDER -s /sbin/nologin $UID_OPT $NAME
-
+echo -e "$PASS\n$PASS" | adduser -h $FOLDER -s /bin/sh  $UID_OPT $NAME
+mkdir -p $FOLDER
 
 chown $NAME:$NAME $FOLDER
 
-mkdir -p $FOLDER
 #run the vsftpd ftp server
 exec /usr/sbin/vsftpd -opasv_min_port=21000 -opasv_max_port=21010 /etc/vsftpd/vsftpd.conf
